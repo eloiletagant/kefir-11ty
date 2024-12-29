64b80668df6b07bb8454e4be12fb2de0896428f8
eloiletagant <anatole.gilet@protonmail.com>
2024-12-29 16:03:08 +0100

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Kefir Club",
+	url: "https://example.com/",
+	language: "fr",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
---
+++ content/blog/probiotique.md
@@ -0,0 +1,7 @@
+---
+title: Probiotique
+description: Définition
+date: 2024-01-03
+---
+
+Les probiotiques sont des micro-organismes dont l'apport comme additif alimentaire est considéré comme bénéfique pour la santé de l'homme outre leur apport nutritionnel.
